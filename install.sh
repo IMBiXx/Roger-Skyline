e30e5c5ce25cfc9d39fb9531b366a77f50c65b4a
Valentin LECART <valecart@student.42.fr>
2019-10-02 17:39:17 +0200

    right order is better

--- install.sh
+++ install.sh
@@ -1,29 +1,36 @@
 echo "$_PURPLE==================================================================$_DEF\n"
-echo "$_PURPLE            debian disk infos :"
-sudo fdisk -l
-
-echo "\n"
-echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            updating..."
+echo "\n"
 apt-get -y update
 apt-get -y upgrade
 
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            installing package..."
+echo "\n"
 apt-get install -y sudo
 apt-get install -y git
 apt-get install -y apache2
 
+echo "\n"
+echo "$_PURPLE==================================================================$_DEF\n"
+echo "$_PURPLE            debian disk infos :"
+echo "\n"
+sudo fdisk -l
+
+echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            installing folder..."
+echo "\n"
 
 cd /root
 git clone https://github.com/IMBiXx/Roger-Skyline.git /root/roger-skyline
 
+echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            user creation..."
 echo "\n"
+
 echo "Adding sudo user... Username ? (default: 'roger')"
 read Username
 Username=${Username:-"roger"}
@@ -33,6 +40,7 @@ sudo adduser $Username sudo
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            INTERFACES"
+echo "\n"
 
 cp /etc/network/interfaces /etc/network/interfaces_save
 rm -f /etc/network/interfaces
@@ -45,6 +53,7 @@ sudo service networking restart
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            SSHD_CONFIG"
+echo "\n"
 
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_save
 rm -rf /etc/ssh/sshd_config
@@ -55,30 +64,35 @@ cp /root/roger-skyline/files/sshd_config /etc/ssh/
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            FIREWALL"
+echo "\n"
 
 sh /root/roger-skyline/scripts/firewall.sh
 
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            DDOS PROTECTION"
+echo "\n"
 
 sh /root/roger-skyline/scripts/ddos.sh
 
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            PORTS SCAN"
+echo "\n"
 
 sh /root/roger-skyline/scripts/ports.sh
 
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            making the configuration persistent..."
+echo "\n"
 
 apt-get install -y iptables-persistent
 
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            UPDATE SCRIPT"
+echo "\n"
 
 mkdir /root/scripts
 cp /root/roger-skyline/scripts/script_log.sh /root/scripts/
@@ -94,6 +108,7 @@ echo "@reboot root /root/scripts/script_log.sh\n" >> /var/spool/cron/crontabs/ro
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            CRONTAB SCRIPT"
+echo "\n"
 
 cp /root/roger-skyline/scripts/script_crontab.sh /root/scripts/
 chmod 755 /root/scripts/script_crontab.sh
@@ -105,12 +120,14 @@ echo "0 0 * * * root /root/scripts/script_crontab.sh\n" >> /var/spool/cron/cront
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            WEB SERVER"
+echo "\n"
 
 systemctl start apache2
 
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            VIRTUAL HOST"
+echo "\n"
 
 mkdir -p /var/www/init.login.fr/html
 chown -R $Username:$Username /var/www/init.login.fr/html
@@ -126,6 +143,7 @@ ln -s /etc/apache2/sites-available/init.login.fr.conf /etc/apache2/sites-enabled
 echo "\n"
 echo "$_PURPLE==================================================================$_DEF\n"
 echo "$_PURPLE            SSL CERTIFICAT"
+echo "\n"
 
 cd /etc/ssl/certs/
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout roger.key -out roger.crt
