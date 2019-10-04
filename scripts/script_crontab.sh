93e2b77c99f574da3f70606d3eec2e827b3071bf
Valentin LECART <valecart@student.42.fr>
2019-10-04 16:52:44 +0200

    crontab watch done

--- scripts/script_crontab.sh
+++ scripts/script_crontab.sh
@@ -3,7 +3,9 @@
 cat /etc/crontab > /root/scripts/new
 DIFF=$(diff new tmp)
 if [ "$DIFF" != "" ]; then
-	echo "Subject: The crontab file has been modified !" | sudo sendmail -v valecart@student.42.fr
+	cp mail_type.txt mail.txt
+	diff new tmp >> mail.txt
+	sudo sendmail -vt < ./mail.txt
 	rm -f /root/scripts/tmp
 	cp /root/scripts/new /root/scripts/tmp
 fi
